72c444c5148098599746d349915ccd79a66d0965
Eric Zhang <ekzhang1@gmail.com>
2022-02-16 02:29:51 -0500

    Add tracing and subscriber

--- crates/sshx-server/src/http.rs
+++ crates/sshx-server/src/http.rs
@@ -1,8 +1,11 @@
 //! HTTP and WebSocket handlers for the sshx web interface.
 
 use axum::{body::Body, routing::get, Router};
+use tower_http::trace::TraceLayer;
 
 /// Returns the web application server, built with Axum.
 pub fn app() -> Router<Body> {
-    Router::new().route("/", get(|| async { "Hello, world!" }))
+    Router::new()
+        .route("/", get(|| async { "Hello, world!" }))
+        .layer(TraceLayer::new_for_http())
 }
--- crates/sshx-server/src/main.rs
+++ crates/sshx-server/src/main.rs
@@ -3,16 +3,20 @@ use tokio::signal::unix::{signal, SignalKind};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
     let addr = "[::1]:8051".parse()?;
 
     let mut sigterm = signal(SignalKind::terminate())?;
     let mut sigint = signal(SignalKind::interrupt())?;
 
+    tracing::info!("server listening at {addr}");
     make_server(&addr, async {
         tokio::select! {
             _ = sigterm.recv() => (),
             _ = sigint.recv() => (),
         }
+        tracing::info!("gracefully shutting down...");
     })
     .await?;
     Ok(())

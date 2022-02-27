28474729575a36f4a5fc176a81f3debef64c496d
Eric Zhang <ekzhang1@gmail.com>
2022-02-27 02:16:04 -0500

    Refactor code, remove fcntl, simplify concepts

--- crates/sshx/src/main.rs
+++ crates/sshx/src/main.rs
@@ -1,98 +1,136 @@
+use std::env;
+use std::io::Read;
 use std::os::unix::io::{FromRawFd, RawFd};
-use std::process::{Command, ExitStatus};
+use std::pin::Pin;
+use std::process::{Child, Command};
 use std::sync::Arc;
-use std::time::Duration;
-use std::{env, thread};
+use std::task::{Context, Poll};
 
 use anyhow::Result;
-use nix::fcntl::{fcntl, FcntlArg, OFlag};
 use nix::pty;
+use pin_project::{pin_project, pinned_drop};
 use tokio::fs::File;
-use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::io::{self, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 use tokio::sync::mpsc;
-use tokio::time;
 
 /// Returns the default shell on this system.
 fn get_default_shell() -> String {
     env::var("SHELL").unwrap_or_else(|_| String::from("/bin/bash"))
 }
 
-/// Entry point for the child process, which spawns a shell.
-fn child_task(shell: &str, slave_port: RawFd) -> Result<ExitStatus> {
-    // Safety: The slave file descriptor was created by openpty() and has its
-    // ownership transferred here. It is closed at the end of the function.
-    let slave = unsafe { std::fs::File::from_raw_fd(slave_port) };
-
-    Command::new(shell)
-        .stdin(slave.try_clone()?)
-        .stdout(slave.try_clone()?)
-        .stderr(slave)
-        .status()
-        .map_err(|e| e.into())
+/// An object that stores the state for a terminal session.
+#[pin_project(PinnedDrop)]
+pub struct Terminal {
+    child: Child,
+    #[pin]
+    master_read: File,
+    #[pin]
+    master_write: File,
 }
 
-/// Entry point for the asynchronous controller.
+impl Terminal {
+    /// Create a new terminal, with attached PTY.
+    pub async fn new(shell: &str) -> Result<Terminal> {
+        let result = pty::openpty(None, None)?;
+
+        // Safety: The slave file descriptor was created by openpty() and has its
+        // ownership transferred here. It is closed at the end of the function.
+        let child = unsafe { Self::child_task(shell, result.slave) }?;
+
+        // Safety: The master file descriptor was created by openpty() and has its
+        // ownership transferred here. It is closed when the object is dropped.
+        let master_read = unsafe { File::from_raw_fd(result.master) };
+
+        // We need to clone the file object to prevent livelocks in Tokio, when multiple
+        // reads and writes happen concurrently on the same file descriptor. This is a
+        // current limitation of how the `tokio::fs::File` struct is implemented, due to
+        // its blocking I/O on a separate thread.
+        let master_write = master_read.try_clone().await?;
+
+        Ok(Self {
+            child,
+            master_read,
+            master_write,
+        })
+    }
+
+    /// Entry point for the child process, which spawns a shell.
+    unsafe fn child_task(shell: &str, slave_port: RawFd) -> Result<Child> {
+        let slave = std::fs::File::from_raw_fd(slave_port);
+
+        Command::new(shell)
+            .stdin(slave.try_clone()?)
+            .stdout(slave.try_clone()?)
+            .stderr(slave)
+            .spawn()
+            .map_err(|e| e.into())
+    }
+}
+
+// Redirect terminal reads to the read file object.
+impl AsyncRead for Terminal {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut io::ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        self.project().master_read.poll_read(cx, buf)
+    }
+}
+
+// Redirect terminal reads to the write file object.
+impl AsyncWrite for Terminal {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        self.project().master_write.poll_write(cx, buf)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.project().master_write.poll_flush(cx)
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.project().master_write.poll_shutdown(cx)
+    }
+}
+
+#[pinned_drop]
+impl PinnedDrop for Terminal {
+    fn drop(self: Pin<&mut Self>) {
+        // Reap the child process on closure so that it doesn't create zombies.
+        self.project().child.kill().ok();
+    }
+}
+
 #[tokio::main]
-async fn controller_task(master_port: RawFd) -> Result<()> {
-    fcntl(master_port, FcntlArg::F_SETFL(OFlag::O_NONBLOCK))?;
+async fn main() -> Result<()> {
+    let shell = get_default_shell();
+    println!("Using default shell: {shell}");
 
-    // Safety: The master file descriptor was created by openpty() and has its
-    // ownership transferred here. It is closed at the end of the function.
-    let mut master = unsafe { File::from_raw_fd(master_port) };
+    let mut terminal = Terminal::new(&shell).await?;
 
-    // Input to communicate with the terminal.
-    let (tx, mut rx) = mpsc::channel::<Arc<[u8]>>(64);
-
-    tokio::spawn(async move {
-        // This task takes ownership of `master`, so there are no issues with
-        // concurrent reads and writes to the same file.
-        let mut buf = [0_u8; 2048];
-        loop {
-            tokio::select! {
-                biased;
-
-                message = rx.recv() => {
-                    if let Some(buf) = message {
-                        master.write_all(&buf[..]).await.expect("Failed to write to master");
-                    } else {
-                        break;
-                    }
-                }
-                result = master.read(&mut buf) => {
-                    match result {
-                        Ok(n) => io::stdout().write_all(&buf[..n]).await.unwrap(),
-                        Err(e) => match e.kind() {
-                            io::ErrorKind::WouldBlock => {
-                                // On EAGAIN (non-blocking read), wait for a little bit.
-                                time::sleep(Duration::from_millis(10)).await;
-                            }
-                            _ => panic!("Failed to read from PTY master: {e}"),
-                        },
-                    }
-                }
-            };
-        }
+    // Separate thread for reading from standard input.
+    let (tx, mut rx) = mpsc::channel::<Arc<[u8]>>(16);
+    tokio::task::spawn_blocking(move || loop {
+        let mut buf = [0_u8; 256];
+        let n = std::io::stdin().read(&mut buf).unwrap();
+        tx.blocking_send(buf[0..n].into()).unwrap();
     });
 
     loop {
         let mut buf = [0_u8; 256];
-        let n = io::stdin().read(&mut buf).await?;
-        tx.send(buf[0..n].into()).await?;
+
+        tokio::select! {
+            Some(bytes) = rx.recv() => {
+                terminal.write_all(&bytes).await?;
+            }
+            result = terminal.read(&mut buf) => {
+                let n = result?;
+                io::stdout().write_all(&buf[..n]).await?;
+            }
+        }
     }
-}
-
-fn main() -> Result<()> {
-    let shell = get_default_shell();
-    println!("Using default shell: {shell}");
-
-    // Safety: Child process spawned by forkpty() does no memory allocation and must
-    // use only "async-signal-safe" functions.
-    let result = pty::openpty(None, None)?;
-    thread::spawn(move || {
-        child_task(&shell, result.slave).ok();
-    });
-
-    controller_task(result.master)?;
-
-    Ok(())
 }

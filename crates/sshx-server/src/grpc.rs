a8baed3e5b9e2274c1f4f66a61faa5c879f56e91
Eric Zhang <ekzhang1@gmail.com>
2022-03-10 00:17:06 -0500

    Add stubs for grpc interface

--- crates/sshx-server/src/grpc.rs
+++ crates/sshx-server/src/grpc.rs
@@ -1,18 +1,45 @@
 //! Defines gRPC routes and application request logic.
 
-use sshx_core::proto::{sshx_service_server::SshxService, HelloReply, HelloRequest};
-use tonic::{Request, Response, Status};
+use sshx_core::proto::{sshx_service_server::SshxService, *};
+use tokio::sync::mpsc;
+use tokio_stream::{wrappers::ReceiverStream, StreamExt};
+use tonic::{Request, Response, Status, Streaming};
 
 /// Server that handles gRPC requests from the sshx command-line client.
 pub struct GrpcServer;
 
 #[tonic::async_trait]
 impl SshxService for GrpcServer {
-    async fn hello(&self, request: Request<HelloRequest>) -> Result<Response<HelloReply>, Status> {
-        let reply = HelloReply {
-            message: format!("Hello {}!", request.get_ref().name),
-        };
+    type ChannelStream = ReceiverStream<Result<ServerUpdate, Status>>;
 
-        Ok(Response::new(reply))
+    async fn open(&self, request: Request<OpenRequest>) -> Result<Response<OpenResponse>, Status> {
+        let _ = request;
+        Ok(Response::new(OpenResponse {
+            name: "placeholder".into(),
+            url: "https://example.com".into(),
+        }))
+    }
+
+    async fn channel(
+        &self,
+        request: Request<Streaming<ClientUpdate>>,
+    ) -> Result<Response<Self::ChannelStream>, Status> {
+        let mut stream = request.into_inner();
+        let (tx, rx) = mpsc::channel(1);
+
+        tokio::spawn(async move {
+            let _ = stream.next().await;
+            let _ = tx;
+        });
+
+        Ok(Response::new(ReceiverStream::new(rx)))
+    }
+
+    async fn close(
+        &self,
+        request: Request<CloseRequest>,
+    ) -> Result<Response<CloseResponse>, Status> {
+        let _ = request;
+        todo!()
     }
 }
--- crates/sshx-server/tests/simple.rs
+++ crates/sshx-server/tests/simple.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use sshx_core::proto::HelloRequest;
+use sshx_core::proto::*;
 
 use crate::common::*;
 
@@ -10,11 +10,9 @@ async fn test_rpc() -> Result<()> {
     let server = TestServer::new()?;
     let mut client = server.grpc_client().await?;
 
-    let req = HelloRequest {
-        name: "adam".into(),
-    };
-    let resp = client.hello(req).await?;
-    assert_eq!(&resp.into_inner().message, "Hello adam!");
+    let req = OpenRequest {};
+    let resp = client.open(req).await?;
+    assert!(!resp.into_inner().name.is_empty());
 
     Ok(())
 }

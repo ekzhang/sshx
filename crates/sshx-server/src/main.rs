893d0d90d4471be18fe95b6c4e37847431ae0a46
Eric Zhang <ekzhang1@gmail.com>
2022-03-11 10:47:05 -0500

    Add clap to the main server with `--host` flag

--- crates/sshx-server/src/main.rs
+++ crates/sshx-server/src/main.rs
@@ -1,12 +1,30 @@
+use std::net::SocketAddr;
+
 use anyhow::Result;
+use clap::Parser;
 use sshx_server::make_server_bind;
 use tokio::signal::unix::{signal, SignalKind};
+
+/// The sshx server CLI interface.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Specify port to listen on.
+    #[clap(long, default_value_t = 8051)]
+    port: u16,
+
+    /// Whether to expose the server on all network interfaces.
+    #[clap(long)]
+    host: bool,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let addr = "[::1]:8051".parse()?;
+    let args = Args::parse();
+    let host = if args.host { "::" } else { "::1" };
+    let addr = SocketAddr::new(host.parse()?, args.port);
 
     let mut sigterm = signal(SignalKind::terminate())?;
     let mut sigint = signal(SignalKind::interrupt())?;

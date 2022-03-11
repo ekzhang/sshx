1f96ae7c07e2ff88833989c0d11376a44a13414e
Eric Zhang <ekzhang1@gmail.com>
2022-03-10 23:30:11 -0500

    Initialize frontend SvelteKit app with Tailwind

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import "../app.css";
+</script>
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+  <title>Home Page</title>
+</svelte:head>
+
+<main class="p-8">
+  <h1 class="text-3xl">Welcome to SvelteKit</h1>
+  <p>
+    Visit <a class="text-blue-700 hover:underline" href="https://kit.svelte.dev"
+      >kit.svelte.dev</a
+    > to read the documentation.
+  </p>
+</main>

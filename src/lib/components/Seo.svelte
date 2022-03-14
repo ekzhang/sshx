45420d1f8b0bdbf2e4f237d66c50c8d906c2f1f1
Eric Zhang <ekzhang1@gmail.com>
2022-03-13 23:58:13 -0400

    Add SEO component and social image

---
+++ src/lib/components/Seo.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let title: string;
+  export let ogTitle: string | null = null;
+  export let description: string;
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta property="og:title" content={ogTitle ?? title} />
+  <meta name="description" property="og:description" content={description} />
+  <meta property="og:image" content="https://sshx.io/images/social-image.jpg" />
+  <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>

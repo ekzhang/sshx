b171615e510fbb9383fd3ea5fcd1f4a7be666fac
Eric Zhang <ekzhang1@gmail.com>
2022-04-09 13:00:29 -0400

    Pin client exit signal on the stack

--- crates/sshx/src/main.rs
+++ crates/sshx/src/main.rs
@@ -6,8 +6,8 @@ use anyhow::Result;
 use sshx::{get_default_shell, Terminal};
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::signal;
-use tokio::sync::{mpsc, watch};
-use tracing::{info, trace};
+use tokio::sync::mpsc;
+use tracing::{error, info, trace};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -21,21 +21,18 @@ async fn main() -> Result<()> {
     // Separate thread for reading from standard input.
     let (tx, mut rx) = mpsc::channel::<Arc<[u8]>>(16);
     thread::spawn(move || loop {
-        let mut buf = [0_u8; 256];
+        let mut buf = [0u8; 256];
         let n = std::io::stdin().read(&mut buf).unwrap();
         if tx.blocking_send(buf[0..n].into()).is_err() {
             break;
         }
     });
 
-    let (exit_tx, mut exit_rx) = watch::channel(false);
-    tokio::spawn(async move {
-        signal::ctrl_c().await.unwrap();
-        exit_tx.send(true).ok();
-    });
+    let exit_signal = signal::ctrl_c();
+    tokio::pin!(exit_signal);
 
     loop {
-        let mut buf = [0_u8; 256];
+        let mut buf = [0u8; 256];
 
         tokio::select! {
             Some(bytes) = rx.recv() => {
@@ -45,11 +42,12 @@ async fn main() -> Result<()> {
                 let n = result?;
                 io::stdout().write_all(&buf[..n]).await?;
             }
-            Ok(()) = exit_rx.changed() => {
-                if *exit_rx.borrow_and_update() {
-                    trace!("gracefully exiting main");
-                    break;
+            result = &mut exit_signal => {
+                if let Err(err) = result {
+                    error!(?err, "failed to listen for exit signal");
                 }
+                trace!("gracefully exiting main");
+                break;
             }
         }
     }

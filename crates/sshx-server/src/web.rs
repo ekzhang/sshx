64c45e373ab024ffcaa0344589266596aa4089d1
Eric Zhang <ekzhang1@gmail.com>
2022-06-07 23:09:19 -0400

    Add axum middleware for stripping .html trailers

--- crates/sshx-server/src/web.rs
+++ crates/sshx-server/src/web.rs
@@ -3,9 +3,11 @@
 use std::io;
 use std::sync::Arc;
 
+use axum::middleware::{self, Next};
+use axum::response::Redirect;
 use axum::routing::{get, get_service};
 use axum::{extract::Path, Router};
-use hyper::StatusCode;
+use hyper::{Request, StatusCode};
 use tower_http::services::{ServeDir, ServeFile};
 use tracing::error;
 
@@ -27,9 +29,38 @@ fn frontend() -> Router {
     let static_files = ServeDir::new("build")
         .precompressed_gzip()
         .precompressed_br()
-        .fallback(root_spa);
+        .not_found_service(root_spa);
 
-    Router::new().nest("/", get_service(static_files).handle_error(error_handler))
+    // Remove unnecessary trailing "/index.html" or ".html" from the static file
+    // path, if it exists. Also translates 404 codes into 200.
+    let remove_dot_html = middleware::from_fn(|req: Request<_>, next: Next<_>| async {
+        let uri = req.uri().clone();
+        let mut resp = next.run(req).await;
+        if resp.status().is_success() {
+            let path = uri.path();
+            if let Some(new_path) = path
+                .strip_suffix("/index.html")
+                .or_else(|| path.strip_suffix(".html"))
+            {
+                let mut location = String::from(new_path);
+                if location.is_empty() {
+                    location += "/";
+                }
+                if let Some(query) = uri.query() {
+                    location += "?";
+                    location += query;
+                }
+                return Err(Redirect::temporary(&location));
+            }
+        } else if resp.status() == StatusCode::NOT_FOUND {
+            *resp.status_mut() = StatusCode::OK;
+        }
+        Ok(resp)
+    });
+
+    Router::new()
+        .nest("/", get_service(static_files).handle_error(error_handler))
+        .layer(remove_dot_html)
 }
 
 /// Runs the backend web API server.

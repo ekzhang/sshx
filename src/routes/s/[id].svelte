90072738ddd7f99a7d8ed7f0c359375c5926c370
Eric Zhang <ekzhang1@gmail.com>
2022-06-11 17:55:38 -0400

    Explore more xterm.js options

--- src/routes/s/[id].svelte
+++ src/routes/s/[id].svelte
@@ -21,12 +21,30 @@
       console.warn("Could not load terminal font", error);
     }
 
-    const term = new Terminal({ fontFamily });
+    const term = new Terminal({
+      allowTransparency: true,
+      cursorBlink: false,
+      cursorStyle: "block",
+      fontFamily,
+      fontWeight: 400,
+      fontWeightBold: 700,
+      scrollback: 5000,
+      theme: {}, // TODO: Add theme
+    });
     term.open(termEl);
     term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
+
+    term.onKey(({ key }) => {
+      console.log(key.charCodeAt(0));
+      term.write(key);
+    });
+
+    term.resize(100, 10);
   });
 </script>
 
 This is the page for session {$page.params.id}.
 
-<div bind:this={termEl} />
+<div>
+  <div class="inline-block" bind:this={termEl} />
+</div>

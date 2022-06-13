b25b47495f8041b5b4d35154b49532d4d39b4e8c
Eric Zhang <ekzhang1@gmail.com>
2022-06-13 10:41:31 -0400

    Disable "remove_dot_html" layer, has blank pages

--- crates/sshx-server/src/web.rs
+++ crates/sshx-server/src/web.rs
@@ -7,11 +7,10 @@ use std::sync::Arc;
 use anyhow::Result;
 use axum::extract::ws::{Message, WebSocket, WebSocketUpgrade};
 use axum::extract::Path;
-use axum::middleware::{self, Next};
-use axum::response::{IntoResponse, Redirect};
+use axum::response::IntoResponse;
 use axum::routing::{get, get_service};
 use axum::{Extension, Router};
-use hyper::{Request, StatusCode};
+use hyper::StatusCode;
 use serde::{Deserialize, Serialize};
 use sshx_core::proto::{server_update::ServerMessage, TerminalData};
 use tokio::sync::mpsc;
@@ -38,38 +37,9 @@ fn frontend() -> Router {
     let static_files = ServeDir::new("build")
         .precompressed_gzip()
         .precompressed_br()
-        .not_found_service(root_spa);
+        .fallback(root_spa);
 
-    // Remove unnecessary trailing "/index.html" or ".html" from the static file
-    // path, if it exists. Also translates 404 codes into 200.
-    let remove_dot_html = middleware::from_fn(|req: Request<_>, next: Next<_>| async {
-        let uri = req.uri().clone();
-        let mut resp = next.run(req).await;
-        if resp.status().is_success() {
-            let path = uri.path();
-            if let Some(new_path) = path
-                .strip_suffix("/index.html")
-                .or_else(|| path.strip_suffix(".html"))
-            {
-                let mut location = String::from(new_path);
-                if location.is_empty() {
-                    location += "/";
-                }
-                if let Some(query) = uri.query() {
-                    location += "?";
-                    location += query;
-                }
-                return Err(Redirect::temporary(&location));
-            }
-        } else if resp.status() == StatusCode::NOT_FOUND {
-            *resp.status_mut() = StatusCode::OK;
-        }
-        Ok(resp)
-    });
-
-    Router::new()
-        .nest("/", get_service(static_files).handle_error(error_handler))
-        .layer(remove_dot_html)
+    Router::new().nest("/", get_service(static_files).handle_error(error_handler))
 }
 
 /// Error handler for tower-http services.

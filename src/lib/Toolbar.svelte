da8ae3321569b0e7133c7895b68b4e63716cd5d5
Eric Zhang <ekzhang1@gmail.com>
2022-07-17 00:21:34 -0400

    Minor toolbar styling polish

--- src/lib/Toolbar.svelte
+++ src/lib/Toolbar.svelte
@@ -10,9 +10,9 @@
 </script>
 
 <div
-  class="inline-block border border-zinc-800 bg-zinc-800/50 backdrop-blur-md rounded-xl p-2"
+  class="inline-block border border-zinc-800 bg-zinc-900/80 backdrop-blur-md rounded-xl p-2"
 >
-  <div class="flex items-center">
+  <div class="flex items-center select-none">
     <a href="/"><img src={logo} alt="sshx logo" class="h-10" /></a>
     <p class="ml-1.5 mr-2 font-medium">sshx</p>
 
@@ -42,7 +42,7 @@
 
 <style lang="postcss">
   .v-divider {
-    @apply h-5 mx-2 border-l-4 border-zinc-700;
+    @apply h-5 mx-2 border-l-4 border-zinc-800;
   }
 
   .icon-button {

f3f5ac5a8140d04f0bc5f79d12da4493b8168735
Cveinnt <wuws.vincent@gmail.com>
2023-11-06 04:31:15 -0800

    Basic palette selector

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -1,10 +1,13 @@
 import { persisted } from "svelte-persisted-store";
+import type themes from "./ui/themes.ts";
 
 export type SettingsStore = {
   name: string;
+  theme: keyof typeof themes;
 };
 
 /** A persisted store for settings of the current user. */
 export const settings = persisted<SettingsStore>("sshx-settings-store", {
   name: "",
+  theme: "defaultDark",
 });

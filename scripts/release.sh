3ab02778b4ad499631eed921ee7523e55bf34839
aokblast <aokblast@FreeBSD.org>
2024-01-05 04:34:50 +0800

    feat: add FreeBSD x86-64 cross compile support

--- scripts/release.sh
+++ scripts/release.sh
@@ -21,12 +21,15 @@ cargo build --release --target x86_64-apple-darwin
 
 cargo build --release --target aarch64-apple-darwin
 
+cross build --release --target x86_64-unknown-freebsd
+
 temp=$(mktemp)
 targets=(
   x86_64-unknown-linux-musl
   aarch64-unknown-linux-musl
   x86_64-apple-darwin
   aarch64-apple-darwin
+  x86_64-unknown-freebsd
 )
 for target in "${targets[@]}"
 do

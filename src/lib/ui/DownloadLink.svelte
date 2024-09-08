1fcab131f1505ce6580d32c93977a6064942d711
Eric Zhang <ekzhang1@gmail.com>
2024-09-08 04:23:21 -0400

    Remake installation instructions

---
+++ src/lib/ui/DownloadLink.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { ExternalLinkIcon } from "svelte-feather-icons";
+
+  export let href: string;
+</script>
+
+<a
+  {href}
+  class="flex items-baseline gap-1.5 py-0.5 px-1.5 rounded bg-white/[7%] hover:bg-white/[15%] border border-transparent active:border-white/50 text-zinc-300 transition-colors"
+>
+  <span class="text-sm">
+    <slot />
+  </span>
+  <ExternalLinkIcon size="12" class="text-zinc-400" />
+</a>
